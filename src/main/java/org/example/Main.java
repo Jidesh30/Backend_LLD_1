c6c8eb253f9a9191f03d6f16b9e6015d29e278b0
Jidesh30 <jideshsenthil97@gmail.com>
2026-04-29 18:02:11 +0530

    feat: added new exam class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,23 @@
 package org.example;
 
 import org.example.demo.BankAccount;
+import org.example.demo.Exam;
 import org.example.demo.Student;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        Student student = new Student();
-//        student.name = "Jidesh";
-//        student.age = 26;
-//        student.batch = "April 26";
+        Student student = new Student();
+        student.name = "Jidesh";
+        student.age = 26;
+        student.batch = "April 26";
+        Exam exam = new Exam();
+        exam.examId = 101;
+        exam.examName = "Backend OOPS";
+        student.exam = exam;
+
+        Student student1 = new Student(student);
 //
 //        Student student2 = new  Student();
 //        student2.name = "Joe dane";
@@ -18,21 +25,22 @@ public class Main {
 //        student2.batch = "April 25";
 //
 //        System.out.println("Student details:");
-        BankAccount account1 = new BankAccount();
-        account1.ownerName = "Jidesh";
-        account1.balance = 1000;
-        account1.deposit(500);
-        account1.showBal();
+//        BankAccount account1 = new BankAccount();
+//        account1.ownerName = "Jidesh";
+//        account1.balance = 1000;
+//        account1.deposit(500);
+//        account1.showBal();
+//
+//        account1.withdraw(150);
+//        account1.showBal();
+//        account1.printTransaction();
+//        System.out.println("Bankaccount details:");
+//
+//        Student student3 = new Student("John", 26, "May 2026");
+//        Student student4 = new Student(student3);
 
-        account1.withdraw(150);
-        account1.showBal();
-        account1.printTransaction();
-        System.out.println("Bankaccount details:");
-//
-//        Student student3 = new Student();
-//        Student student4 = new Student("John", 26, "May 2026");
-//
-//        System.out.println("Student default details:");
+
+        System.out.println("Student default details:");
 
     }
 }
--- src/main/java/org/example/demo/Student.java
+++ src/main/java/org/example/demo/Student.java
@@ -4,6 +4,7 @@ public class Student {
     public String name;
     public int age;
     public String batch;
+    public Exam exam;
 
     public Student(){
         this.name = "Unknown";
@@ -11,10 +12,19 @@ public class Student {
         this.batch = "Not Assigned";
     }
 
-    public Student(String name, int age, String batch){
+    public Student(String name, int age, String batch, Exam exam){
         this.name = name;
         this.age = age;
         this.batch = batch;
+        this.exam = exam;
+    }
+
+    public Student(Student other){
+        this.name = other.name;
+        this.age = other.age;
+        this.batch = other.batch;
+//        this.exam = other.exam; // shallow copy
+        this.exam = new Exam(other.exam);
     }
 
     void registerForClass(String className){
