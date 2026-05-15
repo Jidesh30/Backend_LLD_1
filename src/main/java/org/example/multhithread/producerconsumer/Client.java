197abfc3d990465ed217a8b1732b8ca90f0730eb
Jidesh30 <jideshsenthil97@gmail.com>
2026-05-15 22:23:08 +0530

    feat: semaphore

---
+++ src/main/java/org/example/multhithread/producerconsumer/Client.java
@@ -0,0 +1,17 @@
+package org.example.multhithread.producerconsumer;
+
+import java.util.concurrent.Semaphore;
+
+public class Client {
+    public static void main(String[] args) {
+        Store store = new Store(10);
+
+        Semaphore producerSemaphore = new Semaphore(10);
+        Semaphore consumerSemaphore = new Semaphore(0);
+
+        for(int i=0; i<100; i++){
+            new Thread(new Producer(store, producerSemaphore, consumerSemaphore)).start();
+            new Thread(new Consumer(store, producerSemaphore, consumerSemaphore)).start();
+        }
+    }
+}
